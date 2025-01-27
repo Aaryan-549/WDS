651fd6e0e9795d75529d0d81e73fe0b313d0ab41
Aaryan-549 <abeniwal_be23@thapar.edu>
2025-01-27 17:02:23 +0530

    fixed FAQ Section

--- src/pages/Components/FAQ.jsx
+++ src/pages/Components/FAQ.jsx
@@ -25,26 +25,33 @@ export default function FAQ() {
       <div className="faq-boxes-wrapper" ref={faqBoxesWrapper}>
         <div className="faq-boxes">
           <div className="faq-box">
-            <h3>Who can participate in the event?</h3>
-            <p>Everyone who is a student of Thapar Institute can participate.</p>
+            <div className="text-4xl">Who can participate in the event?</div>
+            <p className="text-xl mt-4">Everyone who is a student of Thapar Institute can participate.</p>
           </div>
           <div className="faq-box">
-            <h3>What is "The Wolf of Dalal Street" event?</h3>
-            <p>
+            <div className="text-4xl">What is "The Wolf of Dalal Street" event?</div>
+            <p className="text-xl mt-4">
               WDS: The Wolf of Dalal Street!! The grandeur event of ECON CLUB
               which encompasses the glimpse of Dalal Street where the virtual
               trading takes place on hand.
             </p>
           </div>
           <div className="faq-box">
-            <h3>Is there any registration fee?</h3>
-            <p>
-            No, the event is free for all students of TIET.
-            </p>
+            <div  className="text-4xl">Is there any registration fee needed?</div>
+            <p div className="text-xl mt-4">No, the event is free for all students of TIET.</p>
+           
           </div>
           <div className="faq-box">
-            <h3>When and where is the event taking place?</h3>
-            <p>TAN audi on 5th November</p>
+            <div className="text-4xl">When and where is the event taking place?</div>
+            <p className="text-xl mt-4">TAN audi on 8th and 9th February</p>
+          </div>
+          <div className="faq-box">
+            <div className="text-4xl">What all can we expect to learn from the event?</div>
+            <p className="text-xl mt-4">Participants will gain insights into stock fundamentals, investment strategies, market analysis, and risk management.</p>
+          </div>
+          <div className="faq-box">
+            <div className="text-4xl">Is there any prerequisite knowledge needed?</div>
+            <p className="text-xl mt-4">Only a very basic understanding of the stock market will be enough.</p>
           </div>
         </div>
       </div>
