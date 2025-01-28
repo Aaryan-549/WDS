44410aa929c71838ad14d59e309a5b5d926f3b9f
avyadav224466@gmail.com <avyadav224466@gmail.com>
2025-01-28 18:44:10 +0530

    registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Registration from './pages/Components/Registration';
 
 
 const App = () => {
@@ -8,6 +9,8 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Registration />} />
+        
      
 
       </Routes>
