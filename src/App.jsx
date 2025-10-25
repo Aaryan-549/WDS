a4626b15674cc0cb0b41564831441b7b68be7e97
AkshitShandilya <akshitshandilya09@gmail.com>
2025-10-25 16:32:27 +0530

    trying something

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Registration from './pages/Components/Registration';
+import ResponseForm from './pages/Components/Registration';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Registration />} />
+        <Route path="/register" element={<ResponseForm />} />
      
 
       </Routes>
